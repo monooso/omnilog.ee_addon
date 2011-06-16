4f9edbc728aa0d45bdb00536afc06b2b7c180ebe
Stephen Lewis <stephen@experienceinternet.co.uk>
2011-06-16 09:53:04 +0100

    Update OmniLogger to work with Omnilog_entry::notify_admin property.
    Remove separate 'notify_admin' flag from 'log' method.

--- third_party/omnilog/tests/test.omnilogger.php
+++ third_party/omnilog/tests/test.omnilogger.php
@@ -39,6 +39,7 @@ class Test_omnilogger extends Testee_unit_test_case {
 			'addon_name'	=> 'Example Add-on',
 			'date'			=> time() - 100,
 			'message'		=> 'Example log entry.',
+            'notify_admin'  => FALSE,
 			'type'			=> Omnilog_entry::NOTICE
 		);
     }
@@ -76,8 +77,9 @@ class Test_omnilogger extends Testee_unit_test_case {
 	public function test__log__success_with_admin_notification()
 	{
         $log_entry = new Omnilog_entry($this->_log_entry_props);
+        $log_entry->set_notify_admin(TRUE);
 
-        $saved_entry_props = array_merge($this->_log_entry_props, array('log_entry_id' => 10));
+        $saved_entry_props = array_merge($log_entry->to_array(), array('log_entry_id' => 10));
         $saved_entry = new Omnilog_entry($saved_entry_props);
 
         $this->_model->expectOnce('save_entry_to_log', array($log_entry));
@@ -86,7 +88,7 @@ class Test_omnilogger extends Testee_unit_test_case {
 		$this->_model->expectOnce('notify_site_admin_of_log_entry', array($saved_entry));
 		$this->_model->setReturnValue('notify_site_admin_of_log_entry', TRUE);
 
-        $this->assertIdentical(TRUE, Omnilogger::log($log_entry, TRUE));
+        $this->assertIdentical(TRUE, Omnilogger::log($log_entry));
 	}
 
 
@@ -94,13 +96,32 @@ class Test_omnilogger extends Testee_unit_test_case {
 	{
         $exception_message = 'Exception';
         $log_entry = new Omnilog_entry($this->_log_entry_props);
+        $log_entry->set_notify_admin(TRUE);
 
         $this->_model->expectOnce('save_entry_to_log', array($log_entry));
 		$this->_model->expectNever('notify_site_admin_of_log_entry');
 
         $this->_model->throwOn('save_entry_to_log', new Exception($exception_message));
 
-        $this->assertIdentical(FALSE, Omnilogger::log($log_entry, TRUE));
+        $this->assertIdentical(FALSE, Omnilogger::log($log_entry));
+	}
+
+
+	public function test__log__notify_admin_failure()
+	{
+        $log_entry = new Omnilog_entry($this->_log_entry_props);
+        $log_entry->set_notify_admin(TRUE);
+
+        $saved_entry_props = array_merge($log_entry->to_array(), array('log_entry_id' => 10));
+        $saved_entry = new Omnilog_entry($saved_entry_props);
+
+        $this->_model->expectOnce('save_entry_to_log', array($log_entry));
+		$this->_model->setReturnValue('save_entry_to_log', $saved_entry);
+
+		$this->_model->expectOnce('notify_site_admin_of_log_entry', array($saved_entry));
+		$this->_model->setReturnValue('notify_site_admin_of_log_entry', FALSE);
+
+        $this->assertIdentical(FALSE, Omnilogger::log($log_entry));
 	}
 
 
