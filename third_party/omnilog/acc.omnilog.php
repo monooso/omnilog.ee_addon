a895323e5449afce180c22b075df3153150d91d9
Stephen Lewis <stephen@experienceinternet.co.uk>
2011-06-21 20:55:15 +0100

    Implement accessory.

---
+++ third_party/omnilog/acc.omnilog.php
@@ -0,0 +1,92 @@
+<?php if ( ! defined('BASEPATH')) exit('Invalid file request.');
+
+/**
+ * OmniLog accessory.
+ *
+ * @author          Stephen Lewis (http://github.com/experience/)
+ * @copyright       Experience Internet
+ * @package         Omnilog
+ */
+
+class Omnilog_acc {
+
+    private $_ee;
+    private $_model;
+    private $_theme_url;
+
+    public $description;
+    public $id;
+    public $name;
+    public $sections;
+    public $version;
+    
+    
+    /* --------------------------------------------------------------
+     * PUBLIC METHODS
+     * ------------------------------------------------------------ */
+    
+    /**
+     * Constructor.
+     *
+     * @access  public
+     * @return  void
+     */
+    public function __construct()
+    {
+        $this->_ee =& get_instance();
+
+        // Tell EE where to look.
+        $this->_ee->lang->loadfile('omnilog');
+        $this->_ee->load->add_package_path(PATH_THIRD .'omnilog/');
+
+        // Load the model.
+        $this->_ee->load->model('omnilog_model');
+        $this->_model = $this->_ee->omnilog_model;
+
+        // Instance properties.
+        $this->description  = $this->_ee->lang->line('omnilog_module_description');
+        $this->id           = $this->_model->get_package_name();
+        $this->name         = $this->_ee->lang->line('omnilog_module_name');
+        $this->sections     = array();
+        $this->version      = $this->_model->get_package_version();
+
+        // Basic stuff required by every view.
+        $this->_base_qs     = 'C=addons_modules' .AMP .'M=show_module_cp' .AMP .'module=omnilog';
+        $this->_base_url    = BASE .AMP .$this->_base_qs;
+        $this->_theme_url   = $this->_model->get_package_theme_url();
+    }
+
+
+    /**
+     * Sets the accessory sections.
+     *
+     * @access  public
+     * @return  void
+     */
+    public function set_sections()
+    {
+        $this->_theme_url   = $this->_model->get_package_theme_url();
+        $this->_ee->cp->add_to_head('<link rel="stylesheet" type="text/css" href="' .$this->_theme_url .'css/cp.css" />');
+
+        $vars = array('log_entries' => $this->_model->get_log_entries(NULL, 10));
+        $this->sections[$this->_ee->lang->line('hd_log')] = $this->_ee->load->view('accessory', $vars, TRUE);
+    }
+
+
+    /**
+     * Required to automatically update the version number.
+     *
+     * @access  public
+     * @return  bool
+     */
+    public function update()
+    {
+        return TRUE;
+    }
+
+
+}
+
+
+/* End of file      : mcp.omnilog.php */
+/* File location    : third_party/omnilog/mcp.omnilog.php */
