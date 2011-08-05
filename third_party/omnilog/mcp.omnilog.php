f88715a6aef4159837be309d705db40cf5629aa6
Stephen Lewis <stephen@experienceinternet.co.uk>
2011-08-05 11:11:32 +0100

    Update log view and accessory to include custom admin emails.

--- third_party/omnilog/mcp.omnilog.php
+++ third_party/omnilog/mcp.omnilog.php
@@ -29,13 +29,13 @@ class Omnilog_mcp {
     {
         $this->_ee =& get_instance();
 
-        // Load the model.
         $this->_ee->load->model('omnilog_model');
         $this->_model = $this->_ee->omnilog_model;
 
-        // Basic stuff required by every view.
         $this->_base_qs     = 'C=addons_modules' .AMP .'M=show_module_cp' .AMP .'module=omnilog';
         $this->_base_url    = BASE .AMP .$this->_base_qs;
+        $this->_theme_url   = $this->_model->get_package_theme_url();
+
         $this->_ee->cp->set_breadcrumb($this->_base_url, $this->_ee->lang->line('omnilog_module_name'));
     }
 
@@ -60,9 +60,19 @@ class Omnilog_mcp {
      */
     public function log()
     {
+        $common_css = $this->_theme_url .'common/css/cp.css';
+        $theme_css  = $this->_ee->config->item('cp_theme') == 'corporate'
+            ? $this->_theme_url .'corporate/css/cp.css'
+            : $this->_theme_url .'default/css/cp.css';
+
+        $this->_ee->cp->add_to_head('<link rel="stylesheet" type="text/css" href="' .$common_css .'" />');
+        $this->_ee->cp->add_to_head('<link rel="stylesheet" type="text/css" href="' .$theme_css .'" />');
+
         $vars = array(
-            'cp_page_title' => $this->_ee->lang->line('hd_log'),
-            'log_entries'   => $this->_model->get_log_entries()
+            'cp_page_title'     => $this->_ee->lang->line('hd_log'),
+            'log_entries'       => $this->_model->get_log_entries(),
+            'webmaster_email'   => $this->_ee->config->item('webmaster_email')
+
         );
         
         return $this->_ee->load->view('log', $vars, TRUE);
--- third_party/omnilog/tests/test.mcp_omnilog.php
+++ third_party/omnilog/tests/test.mcp_omnilog.php
@@ -8,8 +8,9 @@
  * @package         Omnilog
  */
 
-require_once PATH_THIRD .'omnilog/mcp.omnilog' .EXT;
-require_once PATH_THIRD .'omnilog/tests/mocks/mock.omnilog_model' .EXT;
+require_once PATH_THIRD .'omnilog/mcp.omnilog.php';
+require_once PATH_THIRD .'omnilog/classes/omnilog_entry.php';
+require_once PATH_THIRD .'omnilog/tests/mocks/mock.omnilog_model.php';
 
 class Test_omnilog_mcp extends Testee_unit_test_case {
     
@@ -36,7 +37,33 @@ class Test_omnilog_mcp extends Testee_unit_test_case {
         $this->_model   = $this->_ee->omnilog_model;
         $this->_subject = new Omnilog_mcp();
     }
-    
+
+
+    public function test__log__success()
+    {
+        $log_entries = array('a', 'b', 'c');
+        $page_title = 'Example Page Title';
+        $webmaster  = 'webmaster@website.com';
+
+        $view       = 'log';
+        $view_vars  = array(
+            'cp_page_title'     => $page_title,
+            'log_entries'       => $log_entries,
+            'webmaster_email'   => $webmaster
+        );
+
+        $this->_ee->lang->expectAtLeastOnce('line', array('hd_log'));
+        $this->_ee->lang->setReturnValue('line', $page_title);
+
+        $this->_ee->config->expectOnce('item', array('webmaster_email'));
+        $this->_ee->config->setReturnValue('item', $webmaster, array('webmaster_email'));
+
+        $this->_model->expectOnce('get_log_entries');
+        $this->_model->setReturnValue('get_log_entries', $log_entries);
+
+        $this->_ee->load->expectOnce('view', array($view, $view_vars, TRUE));
+        $this->_subject->log();
+    }
 
 }
 
