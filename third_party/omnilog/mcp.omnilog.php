72bf787c43606efb4ab9729f9bdc3897246cc785
Stephen Lewis <stephen@experienceinternet.co.uk>
2012-02-21 00:29:01 +0000

    Change ->_ee to ->EE.

--- third_party/omnilog/mcp.omnilog.php
+++ third_party/omnilog/mcp.omnilog.php
@@ -10,11 +10,11 @@
 
 class Omnilog_mcp {
 
-  private $_ee;
+  private $EE;
   private $_model;
   private $_theme_url;
-    
-    
+
+
   /* --------------------------------------------------------------
    * PUBLIC METHODS
    * ------------------------------------------------------------ */
@@ -27,10 +27,10 @@ class Omnilog_mcp {
    */
   public function __construct()
   {
-    $this->_ee =& get_instance();
+    $this->EE =& get_instance();
 
-    $this->_ee->load->model('omnilog_model');
-    $this->_model = $this->_ee->omnilog_model;
+    $this->EE->load->model('omnilog_model');
+    $this->_model = $this->EE->omnilog_model;
 
     $this->_base_qs = 'C=addons_modules' .AMP .'M=show_module_cp'
       .AMP .'module=omnilog';
@@ -39,20 +39,20 @@ class Omnilog_mcp {
     $this->_theme_url   = $this->_model->get_package_theme_url();
 
     $common_css = $this->_theme_url .'common/css/cp.css';
-    $theme_css  = $this->_ee->config->item('cp_theme') == 'corporate'
+    $theme_css  = $this->EE->config->item('cp_theme') == 'corporate'
       ? $this->_theme_url .'corporate/css/cp.css'
       : $this->_theme_url .'default/css/cp.css';
 
-    $this->_ee->cp->add_to_head('<link rel="stylesheet" href="'
+    $this->EE->cp->add_to_head('<link rel="stylesheet" href="'
       .$common_css .'" />');
 
-    $this->_ee->cp->add_to_head('<link rel="stylesheet" href="'
+    $this->EE->cp->add_to_head('<link rel="stylesheet" href="'
       .$theme_css .'" />');
 
-    $this->_ee->cp->set_breadcrumb($this->_base_url,
-      $this->_ee->lang->line('omnilog_module_name'));
+    $this->EE->cp->set_breadcrumb($this->_base_url,
+      $this->EE->lang->line('omnilog_module_name'));
 
-    $this->_ee->cp->set_right_nav(array(
+    $this->EE->cp->set_right_nav(array(
       'nav_log'   => $this->_base_url .AMP .'method=log',
       'nav_demo'  => $this->_base_url .AMP .'method=demo'
     ));
@@ -67,17 +67,17 @@ class Omnilog_mcp {
    */
   public function demo()
   {
-    $this->_ee->load->library('javascript');
-    $this->_ee->cp->add_to_foot('<script src="'
+    $this->EE->load->library('javascript');
+    $this->EE->cp->add_to_foot('<script src="'
       .$this->_theme_url .'common/js/cp.js"></script>');
 
     $vars = array(
-      'cp_page_title'   => $this->_ee->lang->line('hd_demo'),
+      'cp_page_title'   => $this->EE->lang->line('hd_demo'),
       'run_demo_url'    => $this->_base_url .AMP .'method=run_demo',
-      'webmaster_email' => $this->_ee->config->item('webmaster_email')
+      'webmaster_email' => $this->EE->config->item('webmaster_email')
     );
     
-    return $this->_ee->load->view('demo', $vars, TRUE);
+    return $this->EE->load->view('demo', $vars, TRUE);
   }
 
 
@@ -104,8 +104,8 @@ class Omnilog_mcp {
     $log_count  = $this->_model->get_log_entries_count();
     $log_limit  = $this->_model->get_default_log_limit();
 
-    $log_start  = valid_int($this->_ee->input->get('start'), 0)
-      ? (int) $this->_ee->input->get('start') : 0;
+    $log_start  = valid_int($this->EE->input->get('start'), 0)
+      ? (int) $this->EE->input->get('start') : 0;
 
     $log_entries = $this->_model->get_log_entries(NULL, $log_limit, $log_start);
 
@@ -118,27 +118,27 @@ class Omnilog_mcp {
       : '';
 
     $vars = array(
-      'cp_page_title'   => $this->_ee->lang->line('hd_log'),
+      'cp_page_title'   => $this->EE->lang->line('hd_log'),
       'log_entries'     => $log_entries,
       'next_url'        => $next_url,
       'previous_url'    => $previous_url,
-      'webmaster_email' => $this->_ee->config->item('webmaster_email')
+      'webmaster_email' => $this->EE->config->item('webmaster_email')
     );
 
     // Language strings required by JS.
-    $this->_ee->load->library('javascript');
+    $this->EE->load->library('javascript');
 
-    $this->_ee->javascript->set_global('omnilog.lang', array(
-      'lblShow' => $this->_ee->lang->line('lbl_show'),
-      'lblHide' => $this->_ee->lang->line('lbl_hide')
+    $this->EE->javascript->set_global('omnilog.lang', array(
+      'lblShow' => $this->EE->lang->line('lbl_show'),
+      'lblHide' => $this->EE->lang->line('lbl_hide')
     ));
 
-    $this->_ee->javascript->compile();
+    $this->EE->javascript->compile();
     
-    $this->_ee->cp->add_to_foot('<script type="text/javascript" src="'
+    $this->EE->cp->add_to_foot('<script type="text/javascript" src="'
       .$this->_theme_url .'common/js/cp.js"></script>');
 
-    return $this->_ee->load->view('log', $vars, TRUE);
+    return $this->EE->load->view('log', $vars, TRUE);
   }
 
 
@@ -154,17 +154,17 @@ class Omnilog_mcp {
 
     if ( ! file_exists(PATH_THIRD .'omnilog/classes/omnilogger' .EXT))
     {
-      $this->_ee->session->set_flashdata('message_failure',
-        $this->_ee->lang->line('demo_flashdata_missing_omnilogger')
+      $this->EE->session->set_flashdata('message_failure',
+        $this->EE->lang->line('demo_flashdata_missing_omnilogger')
       );
 
-      $this->_ee->functions->redirect($redirect_url);
+      $this->EE->functions->redirect($redirect_url);
     }
 
     include_once PATH_THIRD .'omnilog/classes/omnilogger' .EXT;
-    $this->_ee->load->helper('email');
+    $this->EE->load->helper('email');
     
-    $demo   = $this->_ee->input->get('demo');
+    $demo   = $this->EE->input->get('demo');
     $emails = array();
 
     switch ($demo)
@@ -185,14 +185,14 @@ class Omnilog_mcp {
         break;
 
       case 'notify_standard':
-        $emails = array($this->_ee->config->item('webmaster_email'));
+        $emails = array($this->EE->config->item('webmaster_email'));
         $notify = TRUE;
         $type   = Omnilog_entry::ERROR;
         break;
 
       case 'notify_custom':
         $emails     = array();
-        $raw_emails = explode(',', urldecode($this->_ee->input->get('email')));
+        $raw_emails = explode(',', urldecode($this->EE->input->get('email')));
 
         foreach ($raw_emails AS $email)
         {
@@ -211,37 +211,37 @@ class Omnilog_mcp {
 
     if ( ! $type)
     {
-      $this->_ee->session->set_flashdata('message_failure',
-        $this->_ee->lang->line('demo_flashdata_unknown_demo')
+      $this->EE->session->set_flashdata('message_failure',
+        $this->EE->lang->line('demo_flashdata_unknown_demo')
       );
 
-      $this->_ee->functions->redirect($redirect_url);
+      $this->EE->functions->redirect($redirect_url);
     }
 
     $omnilog_entry = new Omnilog_entry(array(
       'addon_name'    => 'OmniLog Demo',
       'admin_emails'  => $emails,
       'date'          => time(),
-      'message'       => $this->_ee->lang->line('demo_message'),
-      'extended_data' => $this->_ee->lang->line('demo_extended_data'),
+      'message'       => $this->EE->lang->line('demo_message'),
+      'extended_data' => $this->EE->lang->line('demo_extended_data'),
       'notify_admin'  => $notify,
       'type'          => $type
     ));
 
     if ( ! Omnilogger::log($omnilog_entry))
     {
-      $this->_ee->session->set_flashdata('message_failure',
-        $this->_ee->lang->line('demo_flashdata_failure')
+      $this->EE->session->set_flashdata('message_failure',
+        $this->EE->lang->line('demo_flashdata_failure')
       );
     }
     else
     {
-      $this->_ee->session->set_flashdata('message_success',
-        $this->_ee->lang->line('demo_flashdata_success')
+      $this->EE->session->set_flashdata('message_success',
+        $this->EE->lang->line('demo_flashdata_success')
       );
     }
 
-    $this->_ee->functions->redirect($redirect_url);
+    $this->EE->functions->redirect($redirect_url);
   }
 
 
