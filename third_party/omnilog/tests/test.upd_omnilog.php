d4e38995394f20da914cbf2cf93b57a6a28ba860
Stephen Lewis <stephen@experienceinternet.co.uk>
2012-02-21 00:04:50 +0000

    Tidy up formatting.

--- third_party/omnilog/tests/test.upd_omnilog.php
+++ third_party/omnilog/tests/test.upd_omnilog.php
@@ -14,56 +14,58 @@ require_once PATH_THIRD .'omnilog/tests/mocks/mock.omnilog_model.php';
 
 class Test_omnilog_upd extends Testee_unit_test_case {
 
-    private $_model;
-    private $_subject;
+  private $_model;
+  private $_subject;
 
 
-    /* --------------------------------------------------------------
-     * PUBLIC METHODS
-     * ------------------------------------------------------------ */
+  /* --------------------------------------------------------------
+   * PUBLIC METHODS
+   * ------------------------------------------------------------ */
 
-    /**
-     * Constructor.
-     *
-     * @access  public
-     * @return  void
-     */
-    public function setUp()
-    {
-        parent::setUp();
+  /**
+   * Constructor.
+   *
+   * @access  public
+   * @return  void
+   */
+  public function setUp()
+  {
+    parent::setUp();
 
-        Mock::generate('Mock_omnilog_model', get_class($this) .'_mock_model');
-        $this->_ee->omnilog_model = $this->_get_mock('model');
-        $this->_model   = $this->_ee->omnilog_model;
-        $this->_subject = new Omnilog_upd();
-    }
-
-
-    public function test__install__success()
-    {
-        $this->_model->expectOnce('install_module');
-        $this->_model->setReturnValue('install_module', 'wibble');  // Should just be passed along.
-        $this->assertIdentical('wibble', $this->_subject->install());
-    }
+    Mock::generate('Mock_omnilog_model', get_class($this) .'_mock_model');
+    $this->_ee->omnilog_model = $this->_get_mock('model');
+    $this->_model   = $this->_ee->omnilog_model;
+    $this->_subject = new Omnilog_upd();
+  }
 
 
-    public function test__uninstall__success()
-    {
-        $this->_model->expectOnce('uninstall_module');
-        $this->_model->setReturnValue('uninstall_module', 'wibble');  // Should just be passed along.
-        $this->assertIdentical('wibble', $this->_subject->uninstall());
-    }
+  public function test__install__success()
+  {
+    $this->_model->expectOnce('install_module');
+    $this->_model->setReturnValue('install_module', 'wibble');
+    $this->assertIdentical('wibble', $this->_subject->install());
+  }
 
 
-    public function test__update__success()
-    {
-        $installed_version  = '1.0.0';
-        $return_value       = 'Huzzah!';        // Should just be passed along.
+  public function test__uninstall__success()
+  {
+    $this->_model->expectOnce('uninstall_module');
+    $this->_model->setReturnValue('uninstall_module', 'wibble');
+    $this->assertIdentical('wibble', $this->_subject->uninstall());
+  }
 
-        $this->_model->expectOnce('update_package', array($installed_version));
-        $this->_model->setReturnValue('update_package', $return_value);
-        $this->assertIdentical($return_value, $this->_subject->update($installed_version));
-    }
+
+  public function test__update__success()
+  {
+    $installed_version  = '1.0.0';
+    $return_value       = 'Huzzah!';    // Should just be passed along.
+
+    $this->_model->expectOnce('update_package', array($installed_version));
+    $this->_model->setReturnValue('update_package', $return_value);
+
+    $this->assertIdentical($return_value,
+      $this->_subject->update($installed_version));
+  }
 
 
 }
