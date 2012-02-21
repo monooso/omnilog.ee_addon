96f30f060333fb3f44feb22e5583e3cd2da63a12
Stephen Lewis <stephen@experienceinternet.co.uk>
2012-02-21 00:39:00 +0000

    Tidy up formatting.

--- third_party/omnilog/classes/omnilogger.php
+++ third_party/omnilog/classes/omnilogger.php
@@ -1,4 +1,4 @@
-<?php if ( ! defined('BASEPATH')) exit('Direct script access is not permitted.');
+<?php if ( ! defined('BASEPATH')) exit('Direct script access not permitted.');
 
 /**
  * OmniLogger class.
@@ -11,43 +11,43 @@
 require_once PATH_THIRD .'omnilog/classes/omnilog_entry' .EXT;
 
 class Omnilogger {
-    
-    /* --------------------------------------------------------------
-     * STATIC METHODS
-     * ------------------------------------------------------------ */
 
-    /**
-     * Adds an entry to the log.
-     *
-     * @access  public
-	 * @param	Omnilog_entry		$entry				The log entry.
-     * @return  bool
-     */
-    public static function log(Omnilog_entry $entry)
+  /* --------------------------------------------------------------
+   * STATIC METHODS
+   * ------------------------------------------------------------ */
+
+  /**
+   * Adds an entry to the log.
+   *
+   * @access  public
+   * @param Omnilog_entry   $entry        The log entry.
+   * @return  bool
+   */
+  public static function log(Omnilog_entry $entry)
+  {
+    $EE =& get_instance();
+    $EE->load->add_package_path(PATH_THIRD .'omnilog/');
+    $EE->load->model('omnilog_model');
+
+    try
     {
-        $ee =& get_instance();
-        $ee->load->add_package_path(PATH_THIRD .'omnilog/');
-        $ee->load->model('omnilog_model');
+      $saved_entry = $EE->omnilog_model->save_entry_to_log($entry);
 
-        try
-        {
-            $saved_entry = $ee->omnilog_model->save_entry_to_log($entry);
+      if ($entry->get_notify_admin() === TRUE)
+      {
+        $EE->omnilog_model->notify_site_admin_of_log_entry($saved_entry);
+      }
 
-            if ($entry->get_notify_admin() === TRUE)
-            {
-                $ee->omnilog_model->notify_site_admin_of_log_entry($saved_entry);
-            }
-
-            return TRUE;
-        }
-        catch (Exception $e)
-        {
-            // Don't try to log the error using OmniLog, it could result in an infinite loop.
-            return FALSE;
-        }
+      return TRUE;
     }
+    catch (Exception $e)
+    {
+      // Don't try to log the error using OmniLog. Remember Inception.
+      return FALSE;
+    }
+  }
 
-    
+
 }
 
 
