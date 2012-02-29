3142986f0e4957bb27be5a012ee0bf944687bceb
Stephen Lewis <stephen@experienceinternet.co.uk>
2012-02-29 23:05:50 +0000

    Create NSM Add-on Updater config.php file.

---
+++ third_party/omnilog/config.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * OmniLog NSM Add-on Updater information.
+ *
+ * @author          Stephen Lewis (http://github.com/experience/)
+ * @copyright       Experience Internet
+ * @package         Omnilog
+ */
+
+$config['name']     = 'OmniLog';
+$config['version']  = '1.3.0';
+$config['nsm_addon_updater']['versions_xml']
+  = 'http://experienceinternet.co.uk/software/feeds/omnilog';
+
+/* End of file      : config.php */
+/* File location    : third_party/omnilog/config.php */
